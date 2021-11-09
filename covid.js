879dca42eaa298cbfbdf20b61d3fbfb732a530e7
AhmadDecoded <68193826+AhmadDecoded@users.noreply.github.com>
2021-11-10 00:11:10 +0500

    Added the feature of searching by country instead of one global data

--- covid.js
+++ covid.js
@@ -1,29 +1,45 @@
 let total = document.querySelector(".total");
 let death = document.querySelector(".death");
 let recov = document.querySelector(".rec");
+let button = document.querySelector(".sButton");
+let input = document.querySelector("[data-city-search]");
+
 
 //Now working with the api
 // let newTotal ;
 // let newDeaths;
 // let newRecov;
+let placeName;
 async function covidTracker()
 {
     const response = await fetch("https://api.covid19api.com/summary");
     const data = await response.json();
-    console.log(data);
-    console.log(data.Global);
-    // newTotal = data.Global.TotalConfirmed;
-    // // console.log(newTotal);
-    // newDeaths = data.Global.TotalDeaths;
-    // // console.log(newDeaths);
-    // newRecov = data.Global.TotalRecovered;
-    // // console.log(newRecov);
-    const { TotalConfirmed, TotalDeaths, TotalRecovered} = data.Global;
-    total.textContent = TotalConfirmed;
-    death.textContent = TotalDeaths;
-    recov.textContent = TotalRecovered;
-    
+ 
 
+    for (let i = 0; i < 193; i++) {
+        if(data.Countries[i].Country.toUpperCase() == input.value.toUpperCase())
+        {
+             total.textContent = data.Countries[i].TotalConfirmed;
+             death.textContent = data.Countries[i].TotalDeaths;
+             recov.textContent = data.Countries[i].TotalRecovered;
+             
+        }
+        
+    }
+    
+    
 }
 
-covidTracker();
+
+button.addEventListener("click", ()=>
+{
+    covidTracker();
+    setTimeout(() => {
+        input.value = '';
+        
+    }, 500);
+    
+})
+
+// console.log(placeName.formatted);
+// console.log(document.querySelector('[data-city-search]').formatted_address);
